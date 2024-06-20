121a7779cc52967345095b3a72e8943119afa95c
Ishan <ishannaik7@gmail.com>
2024-06-20 08:14:13 +0530

    Added the following files: Menu component, CardList component, CategoryList component, Featured component, Pagination component. Also updated the layout and page components to include the new components.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,11 @@
+
 import styles from "./homepage.module.css";
 import Link from "next/link";
 export default function Home() {
   return (
     <div>
       <Link href="/">
-        <h1>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias cumque
-          nostrum quibusdam nihil tempora natus consequuntur ducimus officiis,
-          amet ut consectetur laboriosam commodi officia ratione quod
-          exercitationem sapiente! Nam, distinctio. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Labore ipsum eveniet sint recusandae
-          officia pariatur ea accusamus eaque commodi. Qui facere placeat aut
-          explicabo commodi corporis, aliquid dolorum aperiam exercitationem?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </h1>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias cumque
-        nostrum quibusdam nihil tempora natus consequuntur ducimus officiis,
-        amet ut consectetur laboriosam commodi officia ratione quod
-        exercitationem sapiente! Nam, distinctio. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Labore ipsum eveniet sint recusandae
-        officia pariatur ea accusamus eaque commodi. Qui facere placeat aut
-        explicabo commodi corporis, aliquid dolorum aperiam exercitationem?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, sunt
-        ullam eaque nulla et illum, quo amet autem odio quam mollitia ad
-        molestiae accusantium consequatur fuga dolorum, voluptatem veniam est!
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae itaque
-        ipsa in excepturi atque iure minima, dolores obcaecati consequuntur illo
-        explicabo cupiditate aperiam officiis, laboriosam voluptas. Illo fuga
-        placeat vel! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Quas corrupti fugiat molestias aut provident eaque inventore excepturi
-        iusto quibusdam obcaecati id, maiores repellendus ullam sed ipsa animi
-        voluptate veniam beatae.
+
       </Link>
     </div>
   );
