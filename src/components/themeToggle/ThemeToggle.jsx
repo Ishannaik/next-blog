5c7b72072af8a071dae180ebf887c940ec6664b7
Ishan <ishannaik7@gmail.com>
2024-06-21 10:52:37 +0530

    Added ThemeContext and ThemeProvider to the project. Created a new context to manage the theme of the application. Created a new provider to wrap the application with the ThemeContext. Updated the ThemeToggle component to use the new context. Updated the layout component to use the new context. Updated the authLinks component to use the new context. Updated the global styles to use the new context. Updated the themeToggle module styles to use the new context. Updated the authLinks module styles to use the new context.

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,22 @@
+"use client";
+
 import React from "react";
 import styles from "./ThemeToggle.module.css";
+import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 const ThemeToggle = () => {
-  return <div className={styles.container}>ThemeToggle</div>;
+  const { theme } = useContext(ThemeContext);
+
+  console.log(theme);
+
+  return (
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="" width={14} height={14} />
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt="" width={14} height={14} />
+    </div>
+  );
 };
 
 export default ThemeToggle;
